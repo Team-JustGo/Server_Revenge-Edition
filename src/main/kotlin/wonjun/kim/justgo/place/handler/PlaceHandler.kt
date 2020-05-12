4eec6488b053e186c26af2a8c4e395b6d9b84a5e
LeeJongHyun <dwg7691@gmail.com>
2020-05-12 22:15:35 +0900

    좀 더 정진하세요

--- src/main/kotlin/wonjun/kim/justgo/place/handler/PlaceHandler.kt
+++ src/main/kotlin/wonjun/kim/justgo/place/handler/PlaceHandler.kt
@@ -15,25 +15,22 @@ import wonjun.kim.justgo.place.router.response.PlaceResponse
 
 @Component
 class PlaceHandler(val placeRepository: PlaceRepository) {
-    fun handleSuggestion(serverRequest: ServerRequest): Mono<ServerResponse> {
-        val suggestionRequest = SuggestionRequest.from(serverRequest.queryParams().toSingleValueMap())
 
-        return suggestionRequest.toMono()
-            .flatMapMany { request ->
-                placeRepository.findByLocationNear(
-                    Point(request.departLng, request.departLat),
-                    Distance(request.maxDistance?.toDouble() ?: 5000.0 / 1000, Metrics.KILOMETERS)
-                )
-            }.map {
-                val distance = getDistanceByLatLon(
-                    it.location.x,
-                    it.location.y,
-                    suggestionRequest.departLat,
-                    suggestionRequest.departLng
-                )
+    fun handleSuggestion(serverRequest: ServerRequest) =
+        SuggestionRequest.from(serverRequest.queryParams().toSingleValueMap()).toMono()
+            .flatMap(::findLocation)
+            .flatMap(ServerResponse.ok()::bodyValue)
+            .onErrorResume { ServerResponse.notFound().build() }
 
-                return@map PlaceResponse(it.id, it.tags, distance)
-            }.collectList()
-            .flatMap { ServerResponse.ok().bodyValue(it) }
-    }
+    private fun findLocation(request: SuggestionRequest) =
+        placeRepository.findByLocationNear(
+            Point(request.departLat, request.departLng),
+            Distance(request.maxDistance?.toDouble()?.div(1000) ?: 5.0, Metrics.KILOMETERS)
+        ).map {
+            PlaceResponse(
+                it.id, it.tags,
+                getDistanceByLatLon(it.location.x, it.location.y, request.departLat, request.departLng)
+            )
+        }.collectList()
+
 }
