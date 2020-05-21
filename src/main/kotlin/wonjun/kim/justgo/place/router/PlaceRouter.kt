52306c51965164118d5194023e9bcd6f30e5a1cc
LeeJongHyun <dwg7691@gmail.com>
2020-05-21 16:19:08 +0900

    독스 업그레이드
    리퀘스트 / 리스폰스 표 추가

--- src/main/kotlin/wonjun/kim/justgo/place/router/PlaceRouter.kt
+++ src/main/kotlin/wonjun/kim/justgo/place/router/PlaceRouter.kt
@@ -2,14 +2,13 @@ package wonjun.kim.justgo.place.router
 
 import org.springframework.context.annotation.Bean
 import org.springframework.stereotype.Component
-import org.springframework.web.reactive.function.BodyExtractor
 import org.springframework.web.reactive.function.server.router
 import wonjun.kim.justgo.place.handler.PlaceHandler
 
 @Component
 class PlaceRouter(private val placeHandler: PlaceHandler) {
     @Bean
-    fun getPlace() = router {
+    fun getRouter() = router {
         "/place".nest {
             GET("/suggestion", placeHandler::handleSuggestion)
         }
--- src/test/kotlin/wonjun/kim/justgo/place/router/PlaceRouterTest.kt
+++ src/test/kotlin/wonjun/kim/justgo/place/router/PlaceRouterTest.kt
@@ -7,6 +7,10 @@ import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.context.ApplicationContext
 import org.springframework.restdocs.RestDocumentationContextProvider
 import org.springframework.restdocs.RestDocumentationExtension
+import org.springframework.restdocs.operation.preprocess.Preprocessors.*
+import org.springframework.restdocs.payload.JsonFieldType
+import org.springframework.restdocs.payload.PayloadDocumentation.*
+import org.springframework.restdocs.request.RequestDocumentation.*
 import org.springframework.restdocs.webtestclient.WebTestClientRestDocumentation
 import org.springframework.restdocs.webtestclient.WebTestClientRestDocumentation.document
 import org.springframework.test.web.reactive.server.WebTestClient
@@ -49,6 +53,22 @@ class PlaceRouterTest {
             .expectStatus().isOk
             .expectBodyList(PlaceResponse::class.java)
             .hasSize(2)
-            .consumeWith<WebTestClient.ListBodySpec<PlaceResponse>>(document("placeRouter/200"))
+            .consumeWith<WebTestClient.ListBodySpec<PlaceResponse>>(
+                document(
+                    "placeRouter/200",
+                    preprocessResponse(prettyPrint()),
+                    relaxedRequestParameters(
+                        parameterWithName("departLat").description("출발지의 위도"),
+                        parameterWithName("departLng").description("출발지의 경도"),
+                        parameterWithName("maxDistance").description("최대 거리 (미터)"),
+                        parameterWithName("tags").description("태그 ( , 로 구분)").optional()
+                    ),
+                    responseFields(
+                        fieldWithPath("[].id").type(JsonFieldType.STRING).description("장소 유니크 아이디"),
+                        fieldWithPath("[].tags").type(JsonFieldType.ARRAY).description("장소 태그"),
+                        fieldWithPath("[].distance").type(JsonFieldType.NUMBER).description("출발점으로 부터 장소까지의 거리")
+                    )
+                )
+            )
     }
 }
