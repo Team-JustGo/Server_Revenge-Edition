ce37433edb1854f5fc5253ae27d2d0e79b8c271e
Wonjun Kim <kimjnsjwj@gmail.com>
2020-06-11 22:04:14 +0900

    wip

---
+++ src/main/kotlin/wonjun/kim/justgo/navigation/router/dto/DirectionDTO.kt
@@ -0,0 +1,16 @@
+package wonjun.kim.justgo.navigation.router.dto
+
+data class DirectionDTO (
+
+)
+
+/*
+* {
+*    polyLine: String,
+*    direction: [
+*        {
+*
+*        }
+*    ]
+* }
+* */
